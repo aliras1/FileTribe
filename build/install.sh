d198e0b140c7d7574dd445d2340170875f6fc10c
Laszlo Sari <slaszlo.bme@gmail.com>
2019-03-31 00:33:58 +0100

    could not build abigen --> added it as dependency

--- build/install.sh
+++ build/install.sh
@@ -31,12 +31,6 @@ go get -u github.com/gorilla/mux
 go get -u github.com/ipfs/go-ipfs-api
 go get -u github.com/ugorji/go/codec
 go get -u github.com/miguelmota/go-ethereum-hdwallet
-go get -u github.com/tools/godep
-
-CURRENT_DIR=$PWD
-cd ${GOPATH}/src/github.com/ethereum/go-ethereum
-go install ./cmd/abigen/
-cd ${CURRENT_DIR}
 
 echo [*] Generating abi APIs...
 
